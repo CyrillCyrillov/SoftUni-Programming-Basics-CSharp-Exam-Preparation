3576de7fe524e0d0f3b1760a853805563d218901
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-08 20:02:59 +0200

    Add files via upload

---
+++ Task06_Vet_Parking/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task06_Vet_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            double totalSum = 0;
+            for (int i = 1; i <= days; i++)
+            {
+                double sum = 0;
+                for (int j = 1; j <= hours; j++)
+                {
+                    if (i % 2 == 0 && j % 2 != 0)
+                    {
+                        sum += 2.5;
+                    }
+                    else if (i % 2 != 0 && j % 2 == 0)
+                    {
+                        sum += 1.25;
+                    }
+                    else
+                    {
+                        sum += 1;
+                    }
+                }
+                Console.WriteLine($"Day: {i} - {sum:F2} leva");
+                totalSum += sum;
+            }
+
+            Console.WriteLine($"Total: {totalSum:F2} leva");
+        }
+    }
+}
